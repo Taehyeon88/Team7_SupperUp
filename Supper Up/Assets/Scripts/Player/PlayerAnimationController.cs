741778ed997aca6760250ca45c80f2b961e47978
Taehyeon88 <Taehyeon5376@naver.com>
2025-04-07 18:15:13 +0900

    [Feat] 플레이어상태패턴으로 변경 / 공중,낙하후, 등 이동제한추가
    파쿠르, 착지실패 예외처리 추가필요

---
+++ Supper Up/Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public Animator animator;
+    public PlayerStateMachine playerStateMachine;
+
+    private const string PARAM_IS_MOVING = "IsMoving";
+    private const string PARAM_IS_JUMPING = "IsJumping";
+    private const string PARAM_IS_LANDING = "IsLanding";
+    private const string PARAM_IS_FALLING = "IsFalling";
+    private const string PARAM_IS_SUPPERLANDING = "IsSupperLanding";
+    private const string PARAM_IS_Climbing = "IsClimbing";
+
+
+    public void UpdateAnimationState(PlayerState currentState, PlayerState newState)
+    {
+        if (newState == null) return;
+
+        ClearAllParameter();
+
+        switch (newState)
+        {
+            case MoveState:
+                //기본이동은 아무일도 일어나지 않는다.
+                break;
+            case JumpingState:
+                if (currentState is LandingState || currentState is MoveState) ChangeAnimation("Jumping");
+                else animator.SetBool(PARAM_IS_JUMPING, true);
+                break;
+            case FallingState:
+                if (currentState is MoveState) ChangeAnimation("Falling");
+                else animator.SetBool(PARAM_IS_FALLING, true);
+                break;
+            case LandingState:
+                animator.SetTrigger(PARAM_IS_LANDING);
+                break;
+            case SupperLandingState:
+                animator.SetBool(PARAM_IS_SUPPERLANDING, true);
+                break;
+            case ClimbingState:
+                if (currentState is MoveState || currentState is JumpingState) ChangeAnimation("Climbing");
+                else animator.SetTrigger(PARAM_IS_Climbing);
+                break;
+        }
+    }
+
+    private void ChangeAnimation(string stateName)
+    {
+        animator.CrossFade(stateName, 0.01f, 0);
+    }
+
+    private void ClearAllParameter()
+    {
+        animator.SetBool(PARAM_IS_JUMPING, false);
+        animator.SetBool(PARAM_IS_FALLING, false);
+        animator.SetBool(PARAM_IS_SUPPERLANDING, false);
+    }
+}
--- Supper Up/Assets/Scripts/Player/PlayerController.cs
+++ Supper Up/Assets/Scripts/Player/PlayerController.cs
@@ -1,57 +1,207 @@
-using System.Runtime.CompilerServices;
+using System.Collections;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Camera Settings")]
-    public Camera thirdPersonCamera;      //3인칭
+    public Camera thirdPersonCamera;
+    //내부 변수들
+    private Vector3 movement = Vector3.zero;
 
     //플레이어의 움직임 속도를 설정하는 변수
     [Header("Player Movement")]
-    public MovementController movementController;
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+    public float rotationSpeed = 10f;     //회전속도
+    private float currentSpeed = 0;
+    private float currentRotateSpeed = 0;
+
+    [Header("Player Rotation")]
+    public float velocity = 1;
+    public float max_velocity = 3f;
+    private float min_velocity = 1f;
+    private float speedTimer = 0;
+    //제자리 회전변수
+    private float rotateTimer = 0;
+    public float rotateTime = 3f;
+    public float rotateDegree = 30;
+    Quaternion toRoation = Quaternion.identity;
+    private float moveDegree = 0;
+    public bool onRotate = false;
+
+    [Header("Ground Check Setting")]
+    public float fallingThrexhold = -0.1f;            //떨어지는것으로 간주할 수직 속도 임계값
+    public float groundCheckDistance = 0.3f;
+    public float slopedLimit = 45f;                  //등반 가능 최대 경사
+    public bool isFalling = false;
+    public bool isJumping = false;
+    private bool wasGrounded = false;
+    public bool isLanding = false;
+    public LayerMask groundLayer;
 
     //내부 변수들
-    private Vector3 movement = Vector3.zero;
+    private Rigidbody rb;
+    private Animator playerAnimator;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;          //마우스 커서를 잠그고 숨긴다
-        StartCoroutine(movementController.Falling());
+
+        rb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        currentSpeed = moveSpeed;
+        currentRotateSpeed = rotationSpeed;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) movementController.Jumping();
-
-        movementController.Landing();
-
-        movementController.Rotate();
-
-        if (movementController.IsGrounded())
-        {
-            //Debug.Log("땅이다");
-        }
-
-        //Debug.Log(movementController.isJumping);
-    }
-    private void FixedUpdate()
-    {
-        HandleMovement();
+        Landing();
     }
 
     //플레이어 행동처리 함수
-    public void HandleMovement()
+    public void Move()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");         //좌우 입력(1, -1)
         float moveVertical = Input.GetAxis("Vertical");             //앞뒤 입력(1, -1)
 
-        movementController.Move(moveVertical, moveHorizontal, movement);
+        ConstraintsMove();
+
+        Debug.Log($"{CheckDistance()}, {moveVertical}");
+
+        AdjustSpeed(moveVertical);
+        //애니메이션
+        playerAnimator.SetFloat("FMove", moveVertical * velocity);
+        playerAnimator.SetFloat("RMove", moveHorizontal);
 
         //이동 백터 계산
-        if (!movementController.onRotate) movement = (transform.forward * moveVertical + transform.right * moveHorizontal).normalized;
+        if (!onRotate) movement = (transform.forward * moveVertical + transform.right * moveHorizontal).normalized;
         else movement = transform.forward * moveVertical;
+        moveDegree = movement.magnitude;
 
-        movementController.RotateDiagonal(moveHorizontal, moveVertical, movement);
+        rb.MovePosition(rb.position + movement * (moveSpeed + velocity) * Time.deltaTime);
+
+        RotateDiagonal(moveHorizontal, moveVertical, movement);
+    }
+
+    public void ConstraintsMove() //공중에 있을 시, 이속감소.
+    {
+        if (CheckDistance() > 1.1f)
+        {
+            moveSpeed = 0.1f;
+            velocity = min_velocity;
+            speedTimer = 0;
+        }
+        else moveSpeed = currentSpeed;
+    }
+    //파쿠르, 착지, 슈퍼착지 이후 이동값 초기화
+    public void ResetVelocity()
+    {
+        velocity = min_velocity;
+        speedTimer = 0;
+    }
+
+    //시간이 흐름에 따라 속도를 올려주는 코드
+    private void AdjustSpeed(float moveVertical)
+    {
+        if (Mathf.Abs(moveVertical) > 0.95f)
+        {
+            speedTimer = Mathf.Min(speedTimer += Time.deltaTime * 1.2f, max_velocity);
+        }
+        else
+        {
+            speedTimer = Mathf.Max(speedTimer -= Time.deltaTime * 2f, 0);
+        }
+        velocity = Mathf.Clamp(speedTimer, min_velocity, max_velocity);
+    }
+
+    public void Rotate(bool OnRotateAni)
+    {
+        Vector3 cameraForward = thirdPersonCamera.transform.forward;
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+
+        if (moveDegree > 0.1 && !onRotate)                               //카메라에 따라서 캐릭터회전
+        {
+            toRoation = Quaternion.LookRotation(cameraForward, Vector2.up);
+            rotationSpeed = currentRotateSpeed;
+        }
+
+        float dot = Vector3.Dot(transform.forward, cameraForward);               //회전처리를 할 각구하기
+        float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+
+        if (angle >= rotateDegree && moveDegree < 0.1)                           //n초후에 카메라방향으로 회전
+        {
+            rotateTimer += Time.deltaTime;
+            if (rotateTimer > rotateTime)
+            {
+                toRoation = Quaternion.LookRotation(cameraForward, Vector2.up);
+                rotationSpeed = currentRotateSpeed / (angle / 90);
+                rotateTimer = 0;
+                Vector3 left = -transform.right;
+                float temp = Vector3.Angle(cameraForward, left);
+
+                if (OnRotateAni)
+                {
+                    if (temp <= 90) playerAnimator.SetTrigger("Lturn");
+                    else if (temp <= 180) playerAnimator.SetTrigger("Rturn");
+                }
+            }
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRoation, rotationSpeed * Time.deltaTime);
+    }
+
+    public void RotateDiagonal(float moveHorizontal, float moveVertical, Vector3 movement)
+    {
+        if (!onRotate)
+        {
+            if (velocity < 1.5 || Mathf.Abs(moveHorizontal) < 0.3f || Mathf.Abs(moveVertical) < 0.9f) return;
+            Vector3 temp = movement;
+            if (moveVertical < -0.9f) temp = -temp;
+            toRoation = Quaternion.LookRotation(temp, Vector2.up);
+            rotationSpeed = currentRotateSpeed;
+            onRotate = true;
+        }
+        else
+        {
+            if (Mathf.Abs(moveHorizontal) > 0.9f && Mathf.Abs(moveVertical) > 0.3f) return;
+            onRotate = false;
+        }
+    }
+
+    public void Jumping()
+    {
+        rb.AddForce(Vector3.up * jumpForce + movement * velocity * 2.5f, ForceMode.Impulse);
+    }
+
+    public void SupperLanding()
+    {
+        float fallingSpeed = Mathf.Abs(rb.velocity.y);
+        playerAnimator.SetFloat("LandSpeed", Mathf.Clamp(fallingSpeed / 10, 0.8f, 2));
+        isFalling = false;
+    }
+   public void Landing()
+   {
+        if (IsGrounded() && !wasGrounded && isJumping)
+        {
+            isLanding = true;
+            isJumping = false;
+        }
+        wasGrounded = IsGrounded();
+   }
+    public float CheckDistance()
+    {
+        RaycastHit hit;
+        Vector3 temp = transform.position + Vector3.up * 1;
+        if (Physics.Raycast(temp, Vector3.down, out hit)) return hit.distance;
+        return 10f;
+    }
+
+    public bool IsGrounded()
+    {
+        Vector3 origin = transform.position + Vector3.up * 0.1f;
+        float radius = 0.3f;
+        return Physics.CheckSphere(origin, radius, groundLayer);
     }
 
     private void OnDrawGizmos()
@@ -64,7 +214,7 @@ public class PlayerController : MonoBehaviour
         Debug.DrawRay(transform.position, cameraForward);
 
         Gizmos.color = Color.red;
-        Debug.DrawRay(transform.position, transform.forward);
+        Debug.DrawRay(transform.position + Vector3.up, movement * 2f);
 
 
         Vector3 origin = transform.position + Vector3.up * 0.5f;
