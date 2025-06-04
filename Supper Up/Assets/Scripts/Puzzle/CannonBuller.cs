125a0a16a0263141eaaba32f3d6be77d7004cddf
Taehyeon88 <Taehyeon5376@naver.com>
2025-06-04 12:25:11 +0900

    [Feat] 임시 저장

--- Supper Up/Assets/Scripts/Puzzle/CannonBuller.cs
+++ Supper Up/Assets/Scripts/Puzzle/CannonBuller.cs
@@ -6,6 +6,7 @@ public class CannonBullet : MonoBehaviour
 {
     
     private Rigidbody rb;
+    [SerializeField] private GameObject target;
     void OnEnable()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,5 +21,19 @@ public class CannonBullet : MonoBehaviour
     public void Attack(float force, Vector3 dir)
     {
         rb.AddForce(dir * force, ForceMode.Impulse);
+    }
+
+    private IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //StartCoroutine(DestroyBullet());
+
+        Debug.Log("´ê¾Ò´Ù");
+        Instantiate(target, transform.position, Quaternion.identity);
     }
 }
--- Supper Up/Assets/Scripts/Puzzle/CannonObject.cs
+++ Supper Up/Assets/Scripts/Puzzle/CannonObject.cs
@@ -11,24 +11,31 @@ public class CannonObject : MonoBehaviour
     [SerializeField] private GameObject head;
     [SerializeField] private float rotateSpeed = 1f;
 
-    [SerializeField] private GameObject bulletPf;
+    [SerializeField] private GameObject bullerPf;
     [SerializeField] private float firePower = 2f;
+    [SerializeField] private float intitialPower = 2f;
     [SerializeField] private float fireDuration = 5f;
 
     private const float Maxheight = 3f;
     private float timer = 0f;
 
-    // 내부변수
+    //내부변수
     private PlayerController player;
 
+    private float time = 0f;
+
+    private GameObject text;
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        text = Instantiate(bullerPf);
     }
 
     void Update()
     {
         CheckPlayer();
+
+        CheckFirePos(head.transform.forward * firePower, transform.position, head.transform.forward * intitialPower, 3f, text);
     }
 
     private void CheckPlayer()
@@ -38,7 +45,7 @@ public class CannonObject : MonoBehaviour
         float playerH = player.transform.position.y;
         float cannonH = transform.position.y;
 
-        if (dis > maxDistance || playerH < cannonH - 0.5f || playerH > cannonH + Maxheight) return; // 최대거리 및 높이제한
+        if (dis > maxDistance || playerH < cannonH - 0.5f || playerH > cannonH + Maxheight) return; //최대거리 및 높이제한
 
         Vector3 dir = (player.transform.position - transform.position).normalized;
         float dot = Vector3.Dot(dir, transform.forward);
@@ -47,7 +54,7 @@ public class CannonObject : MonoBehaviour
         if (angle < checkingAngle / 2)
         {
             Debug.Log(angle);
-            SlerpHead();
+            //SlerpHead();
             CheckFire();
         }
     }
@@ -61,9 +68,6 @@ public class CannonObject : MonoBehaviour
         Quaternion lookRot = Quaternion.LookRotation(dir, Vector2.up);
         float t = 1f - Mathf.Exp(-rotateSpeed * Time.deltaTime);
         head.transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, t);
-
-        // 삭제 또는 주석 처리
-        // head.transform.rotation.y = (1 - t); 
     }
 
     private void CheckFire()
@@ -79,10 +83,23 @@ public class CannonObject : MonoBehaviour
     private void Fire()
     {
         Vector3 firePos = head.transform.position + head.transform.forward;
-        Vector3 dir = (player.transform.position - firePos).normalized;
-        GameObject bullet = Instantiate(bulletPf, firePos, Quaternion.identity);
-        bullet.GetComponent<CannonBullet>().Attack(firePower, dir);
+        //Vector3 dir = (player.transform.position - firePos).normalized;
+        GameObject bullet = Instantiate(bullerPf, firePos, Quaternion.identity);
+        bullet.GetComponent<CannonBullet>().Attack(firePower, head.transform.forward);
     }
+
+    private void CheckFirePos(Vector3 externalForce, Vector3 startPos, Vector3 initialVelocity, float mass, GameObject bullet)
+    {
+        time += Time.deltaTime;
+        Vector3 gravity = Physics.gravity;
+        Vector3 acceleration = externalForce / mass + gravity;
+
+        Vector3 newPos = startPos + initialVelocity * time + 0.5f * acceleration * time * time;
+
+        bullet.transform.position = newPos;
+    }
+
+
 
     private Vector3 AngleTodir(float angle)
     {
@@ -92,11 +109,13 @@ public class CannonObject : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Vector3 rightdir = AngleTodir(transform.eulerAngles.y + checkingAngle);
-        Vector3 leftdir = AngleTodir(transform.eulerAngles.y - checkingAngle);
+        //Vector3 rightdir = AngleTodir(transform.eulerAngles.y + checkingAngle);
+        //Vector3 leftdir = AngleTodir(transform.eulerAngles.y - checkingAngle);
 
-        Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.red);
-        Debug.DrawRay(transform.position, rightdir * maxDistance, Color.red);
-        Debug.DrawRay(transform.position, leftdir * maxDistance, Color.red);
+        //Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.red);
+        //Debug.DrawRay(transform.position, rightdir * maxDistance, Color.red);
+        //Debug.DrawRay(transform.position, leftdir * maxDistance, Color.red);
+
+        Debug.DrawRay(head.transform.position, head.transform.forward * 2f, Color.green);
     }
 }
